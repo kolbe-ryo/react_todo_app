9bd6f4240922e5ef51b6ff7684ea11e76344a802
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-02 02:06:01 +0900

    modify formatter

--- src/utils/time-format.tsx
+++ src/utils/time-format.tsx
@@ -1,4 +1,5 @@
-// Date型をYYYY/MM/DD HH:MMに変換
+// Date型をYYYY/MM/DD HH:MMに変換(2024/1/10 1:02)
 export const formatDateToYYYYMMDDHHMM = (date: Date): string => {
-  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  const pad = (num: number): string => num.toString().padStart(2, '0');
+  return `${date.getFullYear()}/${pad(date.getMonth() + 1)}/${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}`;
 };
