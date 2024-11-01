14ad1c37deb1652d1df0d8b080147b930f647e68
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-01 23:35:57 +0900

    remove unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css';
 import TodoListPage from './pages/todo-list-page/todo-list-page';
 
 function App() {
   return (
-    <div className="App">
-      <TodoListPage />
-    </div>
+    <TodoListPage />
   );
 }
 
