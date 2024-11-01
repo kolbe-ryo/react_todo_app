0e34e9c1444d843abf9fb6cbbbdcfd608e4eaab6
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-02 03:19:23 +0900

    create authentication page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import TodoListPage from './pages/todo-list-page/todo-list-page';
+import Authentication from './pages/authentication/authentication';
 
 function App() {
-  return TodoListPage();
+  return Authentication();
 }
 
 export default App;
---
+++ src/pages/authentication/authentication.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import styles from './authentication.module.css';
+
+export const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
+  return (
+    <div className={styles.authContainer}>
+      <h1 className={styles.authTitle}>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form className={styles.authForm}>
+        <input
+          type="email"
+          placeholder="Email"
+          className={styles.authInput}
+        />
+        {isLogin && (
+        <input
+          type="password"
+          placeholder="Password"
+          className={styles.authInput}
+        />
+        )}
+        <button type="submit" className={styles.authButton}>
+          {isLogin ? 'Login' : 'Sign Up'}
+        </button>
+      </form>
+      <button onClick={toggleForm} className={styles.toggleButton}>
+        {isLogin ? "Don't have an account? Sign Up" : "Already have an account? Login"}
+      </button>
+    </div>
+  );
+};
+
+export default AuthForm;
