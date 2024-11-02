580103f3d72a12580c1bde49360e16cd3e59b5a5
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-02 15:58:13 +0900

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import TodoListPage from './presentation/pages/todo-list-page/todo-list-page';
 import AuthenticationPage from './presentation/pages/authentication/authentication';
+import NotFoundPage from "./presentation/pages/not-found/not-found-page";
 
 function App() {
   return (
-  <div>
-    <Routes>
-      <Route path="/" element={<TodoListPage />} />
-      <Route path="/login" element={<AuthenticationPage />} />
-    </Routes>
-  </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<TodoListPage />} />
+        <Route path="/login" element={<AuthenticationPage />} />
+        <Route path="*" element={ <NotFoundPage /> } /> 
+      </Routes>
+    </div>
   )
 }
 
