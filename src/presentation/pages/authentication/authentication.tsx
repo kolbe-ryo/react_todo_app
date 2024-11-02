b3357b2bf385497033f7dc1c798c6d01f14236a5
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-02 16:23:30 +0900

    logout button

--- src/presentation/pages/authentication/authentication.tsx
+++ src/presentation/pages/authentication/authentication.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import styles from './authentication.module.css';
 
 export const AuthenticationPage = () => {
+
   const [isLogin, setIsLogin] = useState(true);
 
+  // ログインとサインアップの切り替え
   const toggleForm = () => {
     setIsLogin(!isLogin);
   };
@@ -16,20 +18,26 @@ export const AuthenticationPage = () => {
           type="email"
           placeholder="Email"
           className={styles.authInput}
+          required
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          title="有効なメールアドレスを入力してください"
         />
         {isLogin && (
-        <input
-          type="password"
-          placeholder="Password"
-          className={styles.authInput}
-        />
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.authInput}
+            required
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}"
+            title="大文字・小文字・数字・記号を含む8文字以上のパスワードを入力してください"
+          />
         )}
         <button type="submit" className={styles.authButton}>
           {isLogin ? 'Login' : 'Sign Up'}
         </button>
       </form>
       <button onClick={toggleForm} className={styles.toggleButton}>
-        {isLogin ? "Don't have an account? Sign Up" : "Already have an account? Login"}
+        {isLogin ? "アカウントをお持ちでないですか？" : "すでにアカウントをお持ちですか？"}
       </button>
     </div>
   );
--- src/presentation/pages/todo-list-page/header/todo-list-header.tsx
+++ src/presentation/pages/todo-list-page/header/todo-list-header.tsx
@@ -1,14 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import styles from './todo-list-header.module.css';
 import { TbLogout } from "react-icons/tb";
 
 export const TodoListPageHeader = () => {
-    const text = () => console.log("ログアウト");
+    const navigate = useNavigate();
     return (
-        // 右上のヘッダー部分にログアウトアイコンを設置したい場合は、以下のコードを追加してください。
         <div className={styles.header}>
             <h1 className={styles.title}>TODO List</h1>
             <h3 className={styles.subtitle}>タスクを管理するアプリ</h3>
-            <TbLogout className={styles.logout} onClick={text}/>
+            <TbLogout className={styles.logout} onClick={() => navigate('/login')}/>
         </div>
     );
 }
