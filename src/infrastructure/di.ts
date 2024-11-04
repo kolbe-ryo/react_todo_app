edead3769530c5e146464caaccdc3a9fa56b62db
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-04 23:08:26 +0900

    remove unused file

--- src/infrastructure/di.ts
+++ src/infrastructure/di.ts
@@ -2,8 +2,6 @@ import { createContext } from "react";
 import { ITodoRepository } from "../domain/todo/todo-repository";
 import { MockTodoRepository } from "./mock/mock-todo-repository";
 import { IAuthenticationRepository } from "../domain/authentication/authentication-repository";
-import { MockAuthenticationRepository } from "./mock/mock-authentication-repository";
 
 // DIするrepository object
 export const TodoContext = createContext<ITodoRepository>(new MockTodoRepository());
-export const AuthContext = createContext<IAuthenticationRepository>(new MockAuthenticationRepository());
