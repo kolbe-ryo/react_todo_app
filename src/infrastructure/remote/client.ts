3e3fa85d79a14252ef3855e2d1f025e52a87d620
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-04 20:37:42 +0900

    add db type

--- src/infrastructure/remote/client.ts
+++ src/infrastructure/remote/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "./database.types";
 
 const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL ?? "";
 const supabaseAnonKey: string = process.env.REACT_APP_SUPABASE_ANON_KEY ?? "";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
