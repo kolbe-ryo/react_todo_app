68d35058a407ad3c24c76e860a6aaff5a9e1eaeb
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-06 22:15:54 +0900

    add logger for env

--- src/infrastructure/remote/client.ts
+++ src/infrastructure/remote/client.ts
@@ -4,4 +4,7 @@ import type { Database } from "./database.types";
 const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL ?? "";
 const supabaseAnonKey: string = process.env.REACT_APP_SUPABASE_ANON_KEY ?? "";
 
+console.log("Supabase URL:", supabaseUrl); // 確認用
+console.log("Supabase Anon Key:", supabaseAnonKey); // 確認用
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
